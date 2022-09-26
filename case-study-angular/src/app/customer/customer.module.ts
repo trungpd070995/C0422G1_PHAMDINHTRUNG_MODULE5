f61a9092c86c8a01698ac225dcf406f96b1f0d3b
unknown <you@example.com>
2022-09-26 23:38:05 +0700

    up

--- case-study-angular/src/app/customer/customer.module.ts
+++ case-study-angular/src/app/customer/customer.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCreateComponent, CustomerEditComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule
